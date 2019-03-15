0f14f520d7f7a9826b653f550c8b2985826b4e31
TakahashiMichael <43491484+TakahashiMichael@users.noreply.github.com>
2019-03-15 19:24:28 +0900

    全削除

---
+++ Src/FileNameList.h
@@ -0,0 +1,21 @@
+// @FileNameList.h
+
+
+#ifndef FILENAMELIST_H_INCLUDED
+#define FILENAMELIST_H_INCLUDED
+
+
+/*
+* ファイル名に関して,又,ファイルの場所などコロコロ変わるので
+* 何かあればここから変えてください
+* 
+* 
+* 古臭いけど文字列はdefineマクロで<<というかこれしか知らない
+*/
+
+
+#define FILENAME_SAMPLE_ "";
+
+
+
+#endif // !FILENAMELIST_H_INCLUDED
